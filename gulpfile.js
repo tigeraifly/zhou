19b76cbfd6279283b51326c935252afbe8247efa
wxy <1428563478@qq.com>
2019-04-15 09:47:08 +0800

    第二次

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ const fs = require("fs");
 const server = require("gulp-webserver");
 const uglify = require("gulp-uglify");
 
-
+//压缩js文件
 gulp.task("uglify", () => {
-    return gulp.src("./src/js")
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist'))
-})
+        return gulp.src("./src/js")
+            .pipe(uglify())
+            .pipe(gulp.dest('./dist'))
+    })
+    //压缩css文件
 gulp.task("uglify1", () => {
     return gulp.src("./src/css")
         .pipe(uglify())
@@ -17,16 +18,17 @@ gulp.task("uglify1", () => {
 
 
 
+//gulp其服务
 gulp.task("we1", () => {
     return gulp.src(".")
         .pipe(server({
-            prot: 8080,
+            prot: 8080, //端口号8080
             niddleware: (req, res) => {
-                // const srcc = req.
-                res.end("ijrb")
+                res.end("qoi")
             }
         }))
 })
+
 
 
 gulp.task("we", ["we1", "uglify", "uglify1"])
